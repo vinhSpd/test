970c9ce0c050538bcb908f028c054a8f6cbea7d1
vinhSpd <vinhltph23387@fpt.edu.vn>
2022-11-19 20:31:37 +0700

    IIII

---
+++ JavaApplication23/src/dangNhap/dangNhap.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dangNhap;
+
+/**
+ *
+ * @author 84362
+ */
+public class dangNhap {
+    private String taiKhoan;
+    
+}
